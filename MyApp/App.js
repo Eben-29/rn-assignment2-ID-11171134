3993a0c0170a10695152e5aeec4590f17489297d
eben_29 <emtetteh009@st.ug.edu.gh>
2024-05-27 12:14:05 +0000

    initial commit, My App

---
+++ MyApp/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.name}>Ebenezer Martey Tetteh</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+  },
+  name: {
+    fontWeight: 'bold',
+  }
+});
